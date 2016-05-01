ac7f7a3ebe4cbd141151d95f61cf24bedbe76bfc
Brian Engen <engenb@gmail.com>
2016-05-01 15:49:50 -0500

    Setup (#1)
    * initial check in with build definition
    
    * updating/fixing README
    
    * updating appveyor.yml
    
    * updating readme badges
    
    * changing project to PCL

---
+++ src/PubNub.Async.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PubNub.Async.Tests")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9a1a685-cd1d-433a-b0b9-2f65ce593c55")]
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("An async/await PubNub API client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("engenb")]
+[assembly: AssemblyProduct("PubNub.Async")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
