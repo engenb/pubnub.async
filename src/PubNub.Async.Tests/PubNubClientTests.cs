dc2dc26ebe5c00b50d7d22e8f678e091f395c7c2
Brian Engen <engenb@gmail.com>
2016-06-15 12:38:22 -0500

    lunch and learn awesomeness (#6)
    * checking in a broken test
    
    * fixed the test!

---
+++ src/PubNub.Async.Tests/PubNubClientTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PubNub.Async.Models.Channel;
+using Xunit;
+
+namespace PubNub.Async.Tests
+{
+	public class PubNubClientTests
+	{
+		[Fact]
+		public void Secured__Then_SecureChannel()
+		{
+			var channel = new Channel("channel");
+			var subject = new PubNubClient(channel);
+
+			subject.Secured();
+
+			Assert.True(channel.Secured);
+		}
+	}
+}
